68677872d8ea96827d2f2e61adc991e61cc4e1ba
berke <berkebzt@gmail.com>
2023-04-14 16:41:52 +0300

    writings model has been created.

---
+++ .history/blog/models/yazi_20230414162817.py
@@ -0,0 +1,11 @@
+from django.db import models
+from autoslug import AutoSlugField
+from blog.models import CategoryModel
+
+class WriteModel(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    create_time = models.DateTimeField(auto_now_add=True)
+    edit_time = models.DateTimeField(auto_now=True)
+    slug = AutoSlugField(populate_from = 'title', unique=True)
+    categories = models.ManyToManyField()
---
+++ .history/blog/models/yazi_20230414164052.py
@@ -0,0 +1,19 @@
+from django.db import models
+from autoslug import AutoSlugField
+from blog.models import CategoryModel
+from django.contrib.auth.models import User
+
+class WriteModel(models.Model):
+    img = models.models.ImageField( upload_to='writing_images')
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    create_time = models.DateTimeField(auto_now_add=True)
+    edit_time = models.DateTimeField(auto_now=True)
+    slug = AutoSlugField(populate_from = 'title', unique=True)
+    categories = models.ManyToManyField(CategoryModel, related_name='writing')
+    writer = models.ForeignKey(User,on_delete=models.CASCADE, related_name='writings')
+    
+    class Meta:
+        verbose_name='writing'
+        verbose_name_plural='writings'
+        db_name = 'writing'
