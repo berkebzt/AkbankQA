ca741b55300baaa49eadbe04621501017fb805c0
berke <berkebzt@gmail.com>
2023-04-17 13:35:36 +0300

    updates.

---
+++ .history/blog/admin_20230417011257.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+from blog.models import (
+    CategoryModel,WriteModel,CommentModel,CommunicationModel
+    )
+
+
+admin.site.register(CategoryModel)
+
+@admin.register(WriteModel)
+class WritingAdmin(admin.ModelAdmin):
+    search_fields = ('title','content')
+    list_display = (
+        'title','create_time','edit_time'
+    )
+
+
+@admin.register(CommentModel)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('yazan','create_time','edit_time')
+    search_fields = ('yazan__username',)
+    
+   
+@admin.register(CommunicationModel)
+class CommunicationAdmin(admin.ModelAdmin):
+    list_display = ('email','create_time')
+    search_fields = ('email',)
+    
+   
+# Register your models here.
